b8fa674c9aefa53efe83ab9d637a4d1496206317
Vrishabhshri <vrishabhshri@gmail.com>
2025-01-10 15:45:41 -0500

    Initial Commit

---
+++ Synth/SynthApp.swift
@@ -0,0 +1,17 @@
+//
+//  SynthApp.swift
+//  Synth
+//
+//  Created by Vrishabh  on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SynthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SynthTests/SynthTests.swift
@@ -0,0 +1,17 @@
+//
+//  SynthTests.swift
+//  SynthTests
+//
+//  Created by Vrishabh  on 1/10/25.
+//
+
+import Testing
+@testable import Synth
+
+struct SynthTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
